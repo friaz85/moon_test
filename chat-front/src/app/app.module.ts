c28b7c1e4ccfd31f9bc7aebfd15210975e71c7e5
friaz85 <friaz85@gmail.com>
2020-07-22 18:59:54 -0500

    Inicio
    Inicio del proyecto

---
+++ chat-front/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { ChatInboxComponent } from "./chat-inbox/chat-inbox.component";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [AppComponent, ChatInboxComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
